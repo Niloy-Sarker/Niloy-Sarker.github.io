bd62f2dde8742bdad2df7330b1df9986e5828e4a
Niloy Sarker <69155993+Niloy-Sarker@users.noreply.github.com>
2021-05-09 16:32:57 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title> Welcome to Niloy's personal Webpage</title>
+<title> CodeAndroid </title>
+<style type="text/css">
+.wlcm{
+text-align: center;
+font-family: cursive;
+}
+</style>
 </head>
 <body>
-<div1> 
-<h3> Hey!  Welcome to my page!</h3>
-</div1>
+<?php include 'header.php'; ?>
+<div> 
+<h1 class="wlcm"> Hey!  Welcome to my page! </h1>
+</div>
+<div class="wlcm">
+Here you can find some fancy and usefull items. Want to make your android device to a programming kit? Well, I'm here to help you to practice programming in your android device easily.
+<br>
+<h2 class="wlcm"> So lets start with  :</h2>
+<a href="c.php"> <button id="button1"> Install C / C++ In Your Android Device </button> </a>
 </body>
 </html>
